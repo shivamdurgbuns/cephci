10e39d9c11b321f048530aba2f4a478615bf7947
Chebrolu Harika <hchebrol@redhat.com>
2025-02-26 12:47:58 +0530

    Lib changes for inband authentication
    Signed-off-by: Chebrolu Harika <hchebrol@redhat.com>

--- ceph/nvmegw_cli/namespace.py
+++ ceph/nvmegw_cli/namespace.py
@@ -18,6 +18,13 @@ class Namespace(ExecuteCommandMixin):
     def mtls(self, value):
         self._mtls = value
 
+    def set_qos(self, **kwargs):
+        """Set QoS for namespace."""
+        return self.run_nvme_cli("set_qos", **kwargs)
+
+    def change_key(self, **kwargs):
+        return self.run_nvme_cli("change_key", **kwargs)
+
     def add(self, **kwargs):
         """Adds namespace for subsystem."""
         return self.run_nvme_cli("add", **kwargs)
