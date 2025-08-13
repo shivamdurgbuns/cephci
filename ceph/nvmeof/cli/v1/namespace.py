9607dadc58789c7426e735ad6e2598ae89cfdc34
Sunil Kumar Nagaraju <sunnagar@redhat.com>
2025-08-13 09:30:31 +0530

    CLI library for Ceph NVMe CLI Version2
    Signed-off-by: Sunil Kumar Nagaraju <sunnagar@redhat.com>

---
+++ ceph/nvmeof/cli/v1/namespace.py
@@ -0,0 +1,58 @@
+from ceph.nvmeof.cli.v1.execute import ExecuteCommandMixin
+
+
+class Namespace:
+    """NVMeoF Namespace operations."""
+
+    def __init__(self, base: ExecuteCommandMixin) -> None:
+        self.base = base
+        self.name = "namespace"
+
+    def set_qos(self, **kwargs):
+        """Set QoS for namespace."""
+        return self.run_nvme_cli("set_qos", **kwargs)
+
+    def change_key(self, **kwargs):
+        return self.run_nvme_cli("change_key", **kwargs)
+
+    def add(self, **kwargs):
+        """Adds namespace for subsystem."""
+        return self.base.run_nvme_cli(self.name, "add", **kwargs)
+
+    def add_host(self, **kwargs):
+        """Add a host to a namespace."""
+        return self.base.run_nvme_cli(self.name, "add_host", **kwargs)
+
+    def change_load_balancing_group(self, **kwargs):
+        """Change LB Group Id for namespace under subsystem."""
+        return self.base.run_nvme_cli(
+            self.name, "change_load_balancing_group", **kwargs
+        )
+
+    def change_visibility(self, **kwargs):
+        """Change visibility for namespace under subsystem."""
+        return self.base.run_nvme_cli(self.name, "change_visibility", **kwargs)
+
+    def delete(self, **kwargs):
+        """Delete a namespace from a subsystem."""
+        return self.base.run_nvme_cli(self.name, "del", **kwargs)
+
+    def del_host(self, **kwargs):
+        """Delete host from a namespace."""
+        return self.base.run_nvme_cli(self.name, "del_host", **kwargs)
+
+    def get_io_stats(self, **kwargs):
+        """Get IO Stats for namespace."""
+        return self.base.run_nvme_cli(self.name, "get_io_stats", **kwargs)
+
+    def list(self, **kwargs):
+        """Lists namespaces under subsystem."""
+        return self.base.run_nvme_cli(self.name, "list", **kwargs)
+
+    def resize(self, **kwargs):
+        """Resize namespace under subsystem."""
+        return self.base.run_nvme_cli(self.name, "resize", **kwargs)
+
+    def set_qos(self, **kwargs):
+        """Set QoS for a namespace."""
+        return self.base.run_nvme_cli(self.name, "set_qos", **kwargs)
